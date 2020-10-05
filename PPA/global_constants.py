65cc90f70651384571a5f7020eaf8b27b90434d5
Luis Robaina <luisfabian1545@gmail.com>
2020-10-05 19:26:46 -0400

    OCT5 7

--- PPA/global_constants.py
+++ PPA/global_constants.py
@@ -2,22 +2,22 @@ import os
 
 # Algorithm's Constants:
 
-TIME_INCREMENT = 1.0          # Seconds that each action runs for.
-GAMMA = 1.0                   # Discount Factor.
-DESTINATION_STATE = [0,0]     # Coordinates of the destination.
+TIME_INCREMENT = 1.0            # Seconds that each action runs for.
+GAMMA = 1.0                     # Discount Factor.
+DESTINATION_STATE = [0, 0]      # Coordinates of the destination.
 
-DESTINATION_STATE_REWARD = 1  # Reward for reaching the destination state.
-ABANDON_STATE_REWARD = -0.5   # Negative reward (a penalty) if state is too far from destination.
-TIME_REWARD = -0.001          # Negative reward (penalty) for every second that passes.
-LODWC_REWARD = -0.3           # Negative reward (penalty) for Lost of Well Clear.
-TURN_ACTION_REWARD = -0.00001 # Negative reward (penalty) for every turn action.
+DESTINATION_STATE_REWARD = 1    # Reward for reaching the destination state.
+ABANDON_STATE_REWARD = -0.5     # Negative reward (a penalty) if state is too far from destination.
+TIME_REWARD = -0.001            # Negative reward (penalty) for every second that passes.
+LODWC_REWARD = -0.3             # Negative reward (penalty) for Lost of Well Clear.
+TURN_ACTION_REWARD = -0.00001   # Negative reward (penalty) for every turn action.
 
 # Final State Constants:
-DWC_DIST = 2200;              # (ft) Well Clear distance. 
-DESTINATION_DIST_ERROR = 500  # (ft) Max distance from destination consider Destination reached.
-ABANDON_STATE_ERROR = 51660   # (ft) Distance which if exceed results on an abandon state.
+DWC_DIST = 2200;                # (ft) Well Clear distance.
+DESTINATION_DIST_ERROR = 500    # (ft) Max distance from destination consider Destination reached.
+ABANDON_STATE_ERROR = 51660     # (ft) Distance which if exceed results on an abandon state.
 
-ACTIONS = {                   # Actions are in degrees per second.
+ACTIONS = {                     # Actions are in degrees per second.
     'LEFT': -5,
     'NO_TURN': 0,
     'RIGHT': 5
