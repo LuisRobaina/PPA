eee23e0d8b93da966f7ee5a836f615e73c1712bf
Luis Robaina <luisfabian1545@gmail.com>
2020-10-19 13:00:45 -0400

    Script to graph density

---
+++ graph_density.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from scipy.stats import kde
+import numpy as np
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Short sample app')
+parser.add_argument('-n', action="store", dest='file_name', default=False)
+
+args = parser.parse_args()
+
+# Create data: 200 points
+data = np.loadtxt(args.file_name, delimiter=',', unpack=False)
+x, y = data.T
+
+# Evaluate a gaussian kde on a regular grid of nbins x nbins over data extents
+nbins = 300
+k = kde.gaussian_kde([x, y])
+xi, yi = np.mgrid[x.min():x.max():nbins * 1j, y.min():y.max():nbins * 1j]
+zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+
+# Make the plot
+plt.pcolormesh(xi, yi, zi.reshape(xi.shape), cmap=plt.cm.Greens_r)
+plt.colorbar()
+plt.savefig('Density.png')
+
